ded5e855cafb99b5a609a00b81fefb157d2c95cc
nianshu <nianshu2022@qq.com>
2026-05-29 17:29:49 +0800

    Fix Workers http.createServer compatibility

--- server/worker.js
+++ server/worker.js
@@ -1,7 +1,4 @@
-import { createServer } from 'node:http';
 import { httpServerHandler } from 'cloudflare:node';
 import app from './app.js';
 
-const server = createServer(app);
-
-export default httpServerHandler(server);
+export default httpServerHandler(app);
