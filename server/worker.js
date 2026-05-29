4223a34f95bf33fafe5a2eab4bd8b066e7ae2afe
nianshu <nianshu2022@qq.com>
2026-05-29 17:22:18 +0800

    fix: wrap Express app in http.createServer for wrangler httpServerHandler

--- server/worker.js
+++ server/worker.js
@@ -1,4 +1,7 @@
+import { createServer } from 'node:http';
 import { httpServerHandler } from 'cloudflare:node';
 import app from './app.js';
 
-export default httpServerHandler(app);
+const server = createServer(app);
+
+export default httpServerHandler(server);
